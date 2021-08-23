05beb86de89972940bd1acc7af5bfc3ca131b109
Nemobeng <obengnermine@gmail.com>
2021-08-23 23:34:04 +0100

    Improved link

--- src/App.js
+++ src/App.js
@@ -9,16 +9,25 @@ export default function App() {
         <a
           href="https://www.linkedin.com/in/nermine-obeng-62047a171/"
           target="_blank"
+          rel="noreferrer"
         >
           Nermine Obeng
         </a>{" "}
         and is open-sourced on{" "}
-        <a href="https://github.com/Nemobeng/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/Nemobeng/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           {" "}
           GitHub.
         </a>{" "}
         Hosted on{" "}
-        <a href="https://eager-fermat-c8375d.netlify.app/" target="_blank">
+        <a
+          href="https://eager-fermat-c8375d.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Netlify.
         </a>
       </footer>
