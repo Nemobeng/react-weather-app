302e4b2ded77e2360deb41859acc4779b22b3cef
Nemobeng <obengnermine@gmail.com>
2021-08-23 23:30:10 +0100

    Added link to GitHub and LinkedIn profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/nermine-obeng-62047a171/"
+          target="_blank"
+        >
+          Nermine Obeng
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a href="https://github.com/Nemobeng/react-weather-app" target="_blank">
+          {" "}
+          GitHub.
+        </a>{" "}
+        Hosted on{" "}
+        <a href="https://eager-fermat-c8375d.netlify.app/" target="_blank">
+          Netlify.
+        </a>
+      </footer>
+    </div>
+  );
 }
