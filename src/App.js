3e1ea7dc0db86d29fe1e1acb96bfb7803362909b
Nemobeng <obengnermine@gmail.com>
2021-08-24 11:10:54 +0100

    Created footer component and added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import "./App.css";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/nermine-obeng-62047a171/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Nermine Obeng
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/Nemobeng/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          GitHub.
-        </a>{" "}
-        Hosted on{" "}
-        <a
-          href="https://eager-fermat-c8375d.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button type="button" class="btn btn-primary">
+          hi
+        </button>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/nermine-obeng-62047a171/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nermine Obeng
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/Nemobeng/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          GitHub.
+        </a>{" "}
+        Hosted on{" "}
+        <a
+          href="https://eager-fermat-c8375d.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify.
+        </a>
+      </footer>
+    </div>
+  );
+}
